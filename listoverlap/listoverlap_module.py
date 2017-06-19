f4bb66eeced781d940ed1ee9e28d610b20f41e2a
Lidia Bonifert <bonifert.lidia@gmail.com>
2017-06-19 15:04:02 +0200

    Initial commit - listoverlap + years solution

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,9 +1,16 @@
 def listoverlap(list1, list2):
-    return
+    list3 = []
+    for i in list1:
+        if i in list2:
+            if i in list3:
+                pass
+            else:
+                list3.append(i)
+    return list3
 
 
 def main():
-    return
+    print(listoverlap())
 
 
 if __name__ == '__main__':
--- years/years_module.py
+++ years/years_module.py
@@ -2,11 +2,24 @@ import datetime
 
 
 def years(age):
-    return
+    now = datetime.datetime.now()
+    current_year = now.year
+    yearstoremain = 99 - age
+    year_100 = current_year + yearstoremain
+    return year_100
 
 
 def main():
-    return
+    try:
+        name = input("What is your name?\n")
+    except TypeError:
+        print("Please, enter your name!")
+    try:
+        age = float(input("How old are you?\n"))
+    except TypeError:
+        print("Please, enter your age!")
+    age_input = round(years(age))
+    print("Dear", name, "you will be 100 years old in the year of", age_input, ":) .")
 
 
 if __name__ == '__main__':
